36777b47a108bdad23857d3b75aa5d3c3b8b3baf
gfbatista <gilbertoferrari@gmail.com>
2025-07-14 12:24:23 -0300

    chore: add required fields

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # organo
 
  Projeto introdutório para o aprendizado de Front-end, utilizando o React com JavaScript
+
+ ## Para iniciar o projeto
+ `npm start`
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -14,14 +14,19 @@ const Form = () => {
     "Inovação e Gestão",
   ];
 
+  const onSave = (event) => {
+    event.preventDefault();
+    console.log("TESTE");
+  };
+
   return (
     <section className="form">
-      <form>
+      <form onSubmit={onSave}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <TextField label="Nome" placeholder="Digite o seu nome" />
-        <TextField label="Cargo" placeholder="Digite o seu cargo" />
+        <TextField required={true} label="Nome" placeholder="Digite o seu nome" />
+        <TextField required={true} label="Cargo" placeholder="Digite o seu cargo" />
         <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
-        <DropDown label="Time" itens={squads} />
+        <DropDown required={true} label="Time" itens={squads} />
         <Button>Criar Card</Button>
       </form>
     </section>
